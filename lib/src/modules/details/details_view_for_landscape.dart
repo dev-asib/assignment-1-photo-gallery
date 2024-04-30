27b477f78717d68fb21684482250157d9ffc4464
dev_asib <mail.asib.com@gmail.com>
2024-04-30 14:24:53 +0600

    Updated

--- lib/src/modules/details/details_view_for_landscape.dart
+++ lib/src/modules/details/details_view_for_landscape.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_gallery/src/controller/suggestions_controller.dart';
 import 'package:photo_gallery/src/data/global_widgets/app_bar.dart';
@@ -46,97 +47,102 @@ class PhotoDetailsViewForDeviceLandscapeMode extends StatelessWidget {
           customFontWeight: FontWeight.w600,
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              DetailsPhotoView(
-                photoWidth: detailsPhotoWidth,
-                photoHeight: detailsPhotoHeight,
-                photoDecoration: customBoxDecoration(
-                  boxDecorationColor: Colors.green,
-                  boxDecorationRadius: 30,
-                  boxDecorationImageSrc: detailsViewImgSrc,
-                  boxDecorationImageBoxFit: BoxFit.fill,
-                  boxDecorationBoxShadowImageBlurRadius: 35,
-                  boxDecorationBoxShadowImageShadowColor: Colors.black.withOpacity(0.5),
-                  boxDecorationBoxShadowImageSpreedRadius: 0,
-                  boxDecorationBoxShadowImageOffset_X: 5,
-                  boxDecorationBoxShadowImageOffset_Y: 12,
+      body: ListView(
+        shrinkWrap: false,
+        primary: false,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                DetailsPhotoView(
+                  photoWidth: detailsPhotoWidth,
+                  photoHeight: detailsPhotoHeight,
+                  photoDecoration: customBoxDecoration(
+                    boxDecorationColor: Colors.green,
+                    boxDecorationRadius: 30,
+                    boxDecorationImageSrc: detailsViewImgSrc,
+                    boxDecorationImageBoxFit: BoxFit.fill,
+                    boxDecorationBoxShadowImageBlurRadius: 35,
+                    boxDecorationBoxShadowImageShadowColor: Colors.black.withOpacity(0.5),
+                    boxDecorationBoxShadowImageSpreedRadius: 0,
+                    boxDecorationBoxShadowImageOffset_X: 5,
+                    boxDecorationBoxShadowImageOffset_Y: 12,
 
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: screenHeight,
-                width: screenWidth/2,
-                child: Wrap(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        DetailsPhotoTitleAndDescription(
-                          detailsPhotoTitleText: detialsImgTitleText,
-                          detailsPhotoDescription: detailsImgDescriptionText,
-                          detailsPhotoTitleColor: Colors.black,
-                          detailsPhototitleFontSize: 24,
-                          detailsPhototitleFontWeight: FontWeight.normal,
-                          detailsPhotoDescriptionColor: Colors.black,
-                          detailsPhotoDescriptionFontSize: 15,
-                          detailsPhotoDescriptionFontWeight: FontWeight.normal,
-                        ),
-                        Divider(
-                          color: Colors.transparent,
-                        ),
-                        SeeMoreButton(
-                          seeMoreButtonBackgroundColor:
-                          customColors.seeMoreButtonBackgroundColor,
-                          seeMoreButtonForegroundColor:
-                          customColors.seeMoreButtonForegroundColor,
-                          buttonMaximumSizeHeight: 51,
-                          buttonMaximumSizeWidth: screenWidth,
-                          buttonMinimumSizeHeight: 51,
-                          buttonMinimumSizeWidth: screenWidth,
-                          seeMoreButtonTextSize: 20,
-                        ),
-                        Divider(
-                          color: Colors.transparent,
-                        ),
-                        SuggestionText(
-                          suggestionsTextFontSize: 20,
-                        ),
-                        Divider(
-                          color: Colors.transparent,
-                        ),
-                        SuggestionPhotosView(
-                          item_Count: suggestionsPhotoData.suggestionsPhotoData.length,
-                          crossAxis_Spacing: 20,
-                          mainAxis_Spacing: 20,
-                          boxDecorationBorderRadius: 20,
-                          boxDecorationImageBoxFit: BoxFit.fill,
-                          photo_Height: 180,
-                          photo_Width: 180,
-                          gridViewTitleFontSize: 20,
-                          gridViewTitleFontWeight: FontWeight.bold,
-                          customAlignmentInContainerWidget: Alignment.bottomLeft,
-                          paddingInContainerWidget: EdgeInsets.only(left: 20, bottom: 20),
-                          crossAxis_Count: 2,
-                          boxDecorationBoxShadowSuggestionImageBlurRadius: 35,
-                          boxDecorationBoxShadowSuggestionImageSpreedRadius: 0,
-                          boxDecorationBoxShadowSuggestionImageShadowColor: Colors.black.withOpacity(0.5),
-                          boxDecorationBoxShadowSuggestionImageOffset_X: 0,
-                          boxDecorationBoxShadowSuggestionImageOffset_Y: 15,
-                        ),
+                SizedBox(
+                  height: screenHeight,
+                  width: screenWidth/2,
+                  child: Wrap(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          DetailsPhotoTitleAndDescription(
+                            detailsPhotoTitleText: detialsImgTitleText,
+                            detailsPhotoDescription: detailsImgDescriptionText,
+                            detailsPhotoTitleColor: Colors.black,
+                            detailsPhototitleFontSize: 24,
+                            detailsPhototitleFontWeight: FontWeight.normal,
+                            detailsPhotoDescriptionColor: Colors.black,
+                            detailsPhotoDescriptionFontSize: 15,
+                            detailsPhotoDescriptionFontWeight: FontWeight.normal,
+                          ),
+                          Divider(
+                            color: Colors.transparent,
+                          ),
+                          SeeMoreButton(
+                            seeMoreButtonBackgroundColor:
+                            customColors.seeMoreButtonBackgroundColor,
+                            seeMoreButtonForegroundColor:
+                            customColors.seeMoreButtonForegroundColor,
+                            buttonMaximumSizeHeight: 51,
+                            buttonMaximumSizeWidth: screenWidth,
+                            buttonMinimumSizeHeight: 51,
+                            buttonMinimumSizeWidth: screenWidth,
+                            seeMoreButtonTextSize: 20,
+                          ),
+                          Divider(
+                            color: Colors.transparent,
+                          ),
+                          SuggestionText(
+                            suggestionsTextFontSize: 20,
+                          ),
+                          Divider(
+                            color: Colors.transparent,
+                          ),
+                          SuggestionPhotosView(
+                            item_Count: suggestionsPhotoData.suggestionsPhotoData.length,
+                            crossAxis_Spacing: 20,
+                            mainAxis_Spacing: 20,
+                            boxDecorationBorderRadius: 20,
+                            boxDecorationImageBoxFit: BoxFit.fill,
+                            photo_Height: 180,
+                            photo_Width: 180,
+                            gridViewTitleFontSize: 20,
+                            gridViewTitleFontWeight: FontWeight.bold,
+                            customAlignmentInContainerWidget: Alignment.bottomLeft,
+                            paddingInContainerWidget: EdgeInsets.only(left: 20, bottom: 20),
+                            crossAxis_Count: 2,
+                            boxDecorationBoxShadowSuggestionImageBlurRadius: 35,
+                            boxDecorationBoxShadowSuggestionImageSpreedRadius: 0,
+                            boxDecorationBoxShadowSuggestionImageShadowColor: Colors.black.withOpacity(0.5),
+                            boxDecorationBoxShadowSuggestionImageOffset_X: 0,
+                            boxDecorationBoxShadowSuggestionImageOffset_Y: 15,
+                          ),
 
-                      ],
-                    )],
+                        ],
+                      )],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+          SizedBox(height: 50),
+        ],
       ),
     );
   }
